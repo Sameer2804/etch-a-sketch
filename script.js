b0054267d379d4483e239c78fe65ceda4bc60fa4
Sameer <sameer.ahmed1@kpmg.co.uk>
2023-12-15 16:55:19 +0000

    added 'clear' button that works

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const container = document.querySelector('#container');
 const btn = document.querySelector('#btn-gridChange button')
+const btnClear = document.querySelector('#btn-clear')
 
 createGrid(16);
 
@@ -28,6 +29,10 @@ function createGrid(numberOfGrids){
 
     squares.forEach(square => {
         square.addEventListener('mouseenter', (event) => colorSquare(event, 'red'));
+
+        btnClear.addEventListener('click', (event) => {
+            square.style.backgroundColor = 'white';
+        })
     });
 }
 
@@ -37,7 +42,7 @@ function colorSquare(event, color){
 
 btn.addEventListener('click', () => {
     let numberOfGrids = -1;
-    while(numberOfGrids < 0 || numberOfGrids > 100){
+    while(numberOfGrids < 0 || numberOfGrids >= 100){
         numberOfGrids = +prompt('What number of grids do you want? < 100');
     }
 
